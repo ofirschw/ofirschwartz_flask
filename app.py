951e9f7019c3d3adc78870d0e1d31ae54c7b6ceb
ofirschw <ofirsch@post.bgu.ac.il>
2021-12-04 22:15:07 +0200

    assignment7

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def ;():  # put application's code here
+    return 'Hello to assignment 7!'
 
+
+@app.route('/home')
+def hello_home():  # put application's code here
+    return redirect(url_for('assignment_7'))
+
+@app.route('/contact')
+def contact():  # put application's code here
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
