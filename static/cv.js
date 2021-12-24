0cf16b8951ab8488c581a29265a1cef63a468174
ofirschw <ofirsch@psot.bgu.ac.il>
2021-12-24 16:05:13 +0200

    assignment9

--- static/cv.js
+++ static/cv.js
@@ -1,38 +1,3 @@
-fetch('https://reqres.in/api/users?page=2').then(
-    response => response.json()
-).then(
-    responseJSON => createUserList(responseJSON.data)
-)
-    .catch(
-        err =>console.log(err)
-    );
-
-
-function createUserList(users){
-    console.log(users);
-    const cMain = document.querySelector("main");
-    for(let user of users){
-       const section= document.createElement('section') ;
-       section.innerHTML = `
-           <div>
-             ${user.first_name} ${user.last_name}
-              <br>
-              <a href="mailto:${user.email}">Send email</a>
-           </div> ` ;
-        cMain.appendChild(section);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
 
 function func_name() {
     console.log('ofir schwartz');
