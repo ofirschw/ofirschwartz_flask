5644dffebd8c75bfb131466a7a086baa39d2367f
ofirschw <ofirsch@psot.bgu.ac.il>
2021-12-24 16:04:13 +0200

    assignment9

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask, render_template, redirect, session
+from flask import url_for, request
 
 app = Flask(__name__)
+app.secret_key = '123'
+
 
 @app.route('/')
-def Home_page():  # put application's code here
+def Home_page():
     return render_template('cv.html')
 
+
 @app.route('/assignment8')
-def assignment8():  # put application's code here
+def assignment8():
     return render_template('assignment8.html', user={'name': "Ofir schwartz", 'age': 'twenty'}, hobbies=['soccer', 'surf', 'guitar'])
+
+
+@app.route('/assignment9', methods=['GET', 'POST'])
+def assignment9():
+    search, username, = '', ''
+    user_curr = ({"id": 1, "email": "ofir.schwartz@reqres.in", "first_name": "Ofir", "last_name": "Schwartz"},
+                 {"id": 2, "email": "Bogdan.Planich@reqres.in", "first_name": "Bogdan", "last_name": "Planich"},
+                 {"id": 3, "email": "Dolev.Haziza@reqres.in", "first_name": "Dolev", "last_name": "Haziza"},
+                 {"id": 4, "email": "Omer.Atzili@reqres.in", "first_name": "Omer", "last_name": "Atzili"},
+                 {"id": 5, "email": "Din.David@reqres.in", "first_name": "Din", "last_name": "David"},
+                 {"id": 6, "email": "Cheron.Chery@reqres.in", "first_name": "Cheron", "last_name": "Chery"})
+    curr_method = request.method
+    if curr_method == 'GET':
+        if 'searchInput' in request.args:
+            search = request.args['searchInput']
+            return render_template('assignment9.html', search=search, users=user_curr)
+
+    if curr_method == 'POST':
+        username = request.form['name']
+        session['logged_in'] = True
+        session['username'] = username
+    return render_template('assignment9.html', request_method=request.method, username=username)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session['logged_in'] = False
+    session['username'] = ' '
+    return redirect(url_for('assignment9'))
 
 
 if __name__ == '__main__':
--- static/cv.js
+++ static/cv.js
@@ -1,3 +1,39 @@
+fetch('https://reqres.in/api/users?page=2').then(
+    response => response.json()
+).then(
+    responseJSON => createUserList(responseJSON.data)
+)
+    .catch(
+        err =>console.log(err)
+    );
+
+
+function createUserList(users){
+    console.log(users);
+    const cMain = document.querySelector("main");
+    for(let user of users){
+       const section= document.createElement('section') ;
+       section.innerHTML = `
+           <div>
+             ${user.first_name} ${user.last_name}
+              <br>
+              <a href="mailto:${user.email}">Send email</a>
+           </div> ` ;
+        cMain.appendChild(section);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 function func_name() {
     console.log('ofir schwartz');
 }
@@ -17,5 +53,6 @@ function myFunction() {
     } else {
         document.getElementById("second").innerHTML = "Input of your subject OK";
     }
+
 
 }
